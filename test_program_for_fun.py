3654baa103ad061c59be73de39b983f1116b439c
burmak97 <burmak9714@gmal.com>
2022-03-16 16:57:27 +0300

    test on Linux

---
+++ test_program_for_fun.py
@@ -0,0 +1,5 @@
+import pandas as pd #импортируем библиотеку pandas
+import numpy as np
+from pandas import Series, DataFrame #импортируем объекты Series и DataFrame
+obj = Series([4,7,-5,3]) #простейший способ задания объекта Series
+print(obj)
